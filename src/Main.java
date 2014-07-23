6e92980e551529d8a8c4c0f8829a5de8d529e432
keyle <keyle@capsule.org>
2014-07-23 21:33:07 +1000

    init 3

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,66 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
-    public static String code = "abcdefgh";
+    public static String codesample = "print 'hello world'";
 
     public static void main(String[] args) {
-        lex(code);
+        ArrayList<Token> tokens = lex(codesample);
+        System.out.println(Arrays.toString(tokens.toArray()));
     }
 
-    public static void lex(String str) {
+    public static ArrayList<Token> lex(String str) {
         int pos = 0;
         int line = 1;
+
+        ArrayList<Token> tokens = new ArrayList<>();
+        char[] code = str.toCharArray();
+        int total = code.length;
+
+        while (pos < total) {
+            char c = str.charAt(pos);
+            System.out.println(c);
+
+            if (Character.isLetter(c)) {
+                String tempword = "";
+                int temp = pos;
+
+                while (Character.isLetterOrDigit(code[temp])) {
+                    tempword += code[temp];
+                    temp++;
+                }
+
+                tokens.add(new Token(Tok.AWORD, tempword));
+                pos += temp - pos;
+
+            } else if (Character.isSpaceChar(c)) {
+                pos++;
+            } else {
+                pos++;
+            }
+        }
+
+        return tokens;
     }
 
     public enum Tok {
-        AWORD, STRING, DIGIT, SYMBOL
+        AWORD, STRING_LITERAL, DIGIT_LITERAL, SYMBOL
     }
 
-    public class Token {
+    public static class Token {
+
         Tok type;
         String content;
+
+        public Token(Tok type, String content) {
+            this.type = type;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.type.toString() + " " + this.content + "]";
+        }
     }
 }
