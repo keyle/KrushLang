1fc64352253436798180a1318ac049e59cfec944
keyle <keyle@capsule.org>
2014-07-23 23:34:56 +1000

    init 8

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import java.util.Arrays;
 
 public class Main {
 
+    // :main --
+    // print "hello world"
+    // set bla 3
+    // set bla "word"
+    // :main.loop item --
+    // print item
+    // --
+    // iter bla main.loop
+    // --
     public static void main(String[] args) {
         String codesample = "print \'hello world\' 2.12 5.4";
         ArrayList<Token> tokens = Lexer.lex(codesample);
