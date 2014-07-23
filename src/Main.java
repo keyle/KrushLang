659fba0f52aa2aae2cfcee5bdc8323e85ad32f5d
keyle <keyle@capsule.org>
2014-07-23 20:54:44 +1000

    init 2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,20 @@ public class Main {
     public static String code = "abcdefgh";
 
     public static void main(String[] args) {
-
+        lex(code);
     }
 
     public static void lex(String str) {
+        int pos = 0;
+        int line = 1;
+    }
 
+    public enum Tok {
+        AWORD, STRING, DIGIT, SYMBOL
+    }
+
+    public class Token {
+        Tok type;
+        String content;
     }
 }
