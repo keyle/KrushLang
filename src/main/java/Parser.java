6125847e4f04fe5f9d23ec9a80ffab76c8cf4be8
keyle <keyle@capsule.org>
2014-07-26 22:21:05 +1000

    more on parsing

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Parser {
 
     public static final String CLASS_MISSING_NAME = "Invalid Class Definition, <class Name> missing 'name'";
     public static final String CLASS_MISSING_BEGIN_BLOCK = "Missing Class Begin Block, '{' after class name";
     public static final String CLASS_UNKNOWN_WORD = "Class should contain 'def' and not unknown words";
+    public static final String FUNC_MISSING_NAME = "Invalid Definition, <def Name> missing 'name'";
 
     public static void syntaxicAnalysis(ArrayList<Token> tokens) {
 
@@ -13,7 +15,8 @@ public class Parser {
             Token token = tokens.get(i);
 
             if (token.type == Token.Type.CLASS_KEYWORD) {
-                findClasses(tokens, i);
+                ArrayList<ClassDef> classes = findClasses(tokens, i);
+                System.out.println(Arrays.toString(classes.toArray()));
             }
 
 //            tokens.add(tokens.get(i));
@@ -32,35 +35,49 @@ public class Parser {
             crash(CLASS_MISSING_BEGIN_BLOCK);
         }
 
+        ClassDef c = new ClassDef(tokens.get(k + 1).content);
+
         for (int i = k + 2; i < tokens.size(); i++) {
             Token token = tokens.get(i);
 
             if (token.type == Token.Type.FUNC_KEYWORD) {
-                findFunctions(tokens, i);
+                c.functions = findFunctions(tokens, i);
             }
         }
 
-        ClassDef c = new ClassDef(tokens.get(k + 1).content);
+        classes.add(c);
 
         return classes;
     }
 
-    public static void findFunctions(ArrayList<Token> tokens, int k) {
+    public static ArrayList<FunctionDef> findFunctions(ArrayList<Token> tokens, int k) {
 
         ArrayList<FunctionDef> functions = new ArrayList<>();
 
         int m = findNextTokenTypeFrom(Token.Type.WORD, tokens, k);
 
-//        if (!tokens.get(m).content.equals(KEYWORD_FUNCTION)) {
-//            crash(CLASS_UNKNOWN_WORD);
-//        } else {
-//
-//        }
+        if (m == -1) {
+            crash(CLASS_UNKNOWN_WORD);
+        } else if (tokens.get(m).type != Token.Type.WORD) {
+            crash(FUNC_MISSING_NAME);
+        } else {
+            String funcname = tokens.get(m).content;
+            FunctionDef func = new FunctionDef(funcname);
+            functions.add(func);
+        }
+
+        return functions;
     }
 
     public static int findNextTokenTypeFrom(Token.Type tokenType, ArrayList<Token> tokens, int k) {
 
-        return 0;
+        for (int i = k; i < tokens.size(); i++) {
+            if (tokens.get(i).type == tokenType) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
 
@@ -77,10 +94,24 @@ public class Parser {
         public ClassDef(String name) {
             this.name = name;
         }
+
+        @Override
+        public String toString() {
+            return "\n{c " + this.name + "} \n\t" + functions.toString();
+        }
     }
 
     public static class FunctionDef {
 
         String name;
+
+        public FunctionDef(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "<f " + this.name + ">";
+        }
     }
 }
