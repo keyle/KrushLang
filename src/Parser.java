7484012849e66c737c90026fb7aa651a8396999f
keyle <keyle@capsule.org>
2014-07-26 17:12:44 +1000

    refactor

--- src/Parser.java
+++ src/Parser.java
@@ -8,53 +8,60 @@ public class Parser {
     public static final String CLASS_MISSING_NAME = "Invalid Class Definition, <class Name> missing 'name'";
     public static final String CLASS_MISSING_BEGIN_BLOCK = "Missing Class Begin Block, '{' after class name";
 
-    public static ArrayList<Token> syntaxicAnalysis(ArrayList<Token> tokenList) {
+    public static Object syntaxicAnalysis(ArrayList<Token> tokens) {
 
-        ArrayList<Token> tokens = new ArrayList<>();
+        for (int i = 1; i < tokens.size() - 1; i++) {
 
-        for (int i = 1; i < tokenList.size() - 1; i++) {
-
-            Token token = tokenList.get(i);
+            Token token = tokens.get(i);
 
             if (token.type == Token.Type.WORD && token.content.equals(KEYWORD_CLASS)) {
-                findClasses(tokenList, i);
+                findClasses(tokens, i);
             }
 
-//            tokens.add(tokenList.get(i));
+//            tokens.add(tokens.get(i));
         }
 
-        return tokens;
+        return null;
     }
 
-    public static ArrayList<ClassDef> findClasses(ArrayList<Token> tokenList, int k) {
+    public static ArrayList<ClassDef> findClasses(ArrayList<Token> tokens, int k) {
 
-        ArrayList<ClassDef> classes = new ArrayList<ClassDef>();
+        ArrayList<ClassDef> classes = new ArrayList<>();
 
-        if (tokenList.get(k + 1).type != Token.Type.WORD) {
+        if (tokens.get(k + 1).type != Token.Type.WORD) {
             crash(CLASS_MISSING_NAME);
         }
 
-        if (tokenList.get(k + 2).type != Token.Type.BEGIN_BLOCK) {
+        if (tokens.get(k + 2).type != Token.Type.BEGIN_BLOCK) {
             crash(CLASS_MISSING_BEGIN_BLOCK);
         }
 
-        for (int i = k + 2; i < tokenList.size(); i++) {
-            Token token = tokenList.get(i);
+        for (int i = k + 2; i < tokens.size(); i++) {
+            Token token = tokens.get(i);
 
             if (token.type == Token.Type.WORD && token.content.equals(KEYWORD_FUNCTION)) {
-                findFunctions(tokenList, i);
+                findFunctions(tokens, i);
             }
         }
 
-        ClassDef c = new ClassDef(tokenList.get(k + 1).content);
+        ClassDef c = new ClassDef(tokens.get(k + 1).content);
 
         return classes;
     }
 
-    public static void findFunctions(ArrayList<Token> tokenList, int k) {
+    public static void findFunctions(ArrayList<Token> tokens, int k) {
 
+        ArrayList<FunctionDef> functions = new ArrayList<>();
+
+        int index = findNextTokenTypeFrom(Token.Type.WORD, tokens, k);
 
     }
+
+    public static int findNextTokenTypeFrom(Token.Type tokenType, ArrayList<Token> tokens, int k) {
+
+        return 0;
+    }
+
 
     public static void crash(String reason) {
         System.out.println("Parsing failed: " + reason);
