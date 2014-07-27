72547d94c261b9fec5c365f2c7042caeada54550
keyle <keyle@capsule.org>
2014-07-27 16:52:12 +1000

    return a classdef

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,6 +1,13 @@
 public class Generator {
 
-    public static void generate() {
+    public static final String CLASSCODE = "public class %s {";
 
+    public static void generate(Parser.ClassDef classDef) {
+
+        String fileContent = "";
+        fileContent += String.format(CLASSCODE, classDef.name);
+
+        System.out.println(fileContent);
     }
+
 }
